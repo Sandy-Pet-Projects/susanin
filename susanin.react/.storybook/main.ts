6161bb3e82ccffdee6505419aecb8fc51b3a461f
andToxa <andToxa@gmail.com>
2023-07-22 12:55:38 +0700

    build(frontend): bump package versions

--- susanin.react/.storybook/main.ts
+++ susanin.react/.storybook/main.ts
@@ -11,6 +11,9 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {},
   },
+  core: {
+    disableTelemetry: true, // 👈 Disables telemetry
+  },
   docs: {
     autodocs: "tag",
   },
